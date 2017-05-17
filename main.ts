5b9f36d2dee74fc40e7b486f7fbdebecfe3acabd
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-05-17 10:16:01 -0400

    trying extending Error class

--- main.ts
+++ main.ts
@@ -3,3 +3,11 @@ console.log('in main.ts, Node', process.version)
 const add = (a:number, b:number) => a + b
 
 console.log('10 + 20', add(10, 20))
+
+class NoPackagesError extends Error {
+  constructor(m: string) {
+    super(m)
+    // Set the prototype explicitly
+    Object.setPrototypeOf(this, NoPackagesError.prototype);
+  }
+}
