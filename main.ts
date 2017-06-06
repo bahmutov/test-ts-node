e990a0ff52f1ebf30fe1773b39056184fafb17d2
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-06-06 10:32:43 -0400

    prettier and standard currently conflict in whitespace

--- main.ts
+++ main.ts
@@ -5,7 +5,7 @@ const add = (a: number, b: number) => a + b
 console.log('10 + 20', add(10, 20))
 
 class NoPackagesError extends Error {
-  constructor (m: string) {
+  constructor(m: string) {
     super(m)
     // Set the prototype explicitly
     Object.setPrototypeOf(this, NoPackagesError.prototype)
