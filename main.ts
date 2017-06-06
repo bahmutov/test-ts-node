90247bb362aa0d336114f78fd0fc187813102902
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-06-06 10:41:30 -0400

    linting using prettier-standard + lint-ts

--- main.ts
+++ main.ts
@@ -2,10 +2,14 @@ console.log('in main.ts, Node', process.version)
 
 const add = (a: number, b: number) => a + b
 
+function sub (a: number, b: number) {
+  return a - b
+}
+
 console.log('10 + 20', add(10, 20))
 
 class NoPackagesError extends Error {
-  constructor(m: string) {
+  constructor (m: string) {
     super(m)
     // Set the prototype explicitly
     Object.setPrototypeOf(this, NoPackagesError.prototype)
